6e5d57eca863c4d075f24f059c02f61c15db6a0d
sidhardh55 <sidhumanchukonda@gmail.com>
2026-07-28 19:55:29 +0530

    Time: 99 ms (29.23%), Space: 53.9 MB (99.12%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        string ans = "";
-        if(s.length()==1) return s;
-        map<char,int> mp;
-        for(int i=0;i<=(s.size()/2)-1;i++){
-            mp[s[i]] += 2;
+        int n=s.length();
+        sort(s.begin(),s.begin() + n/2);
+        for(int i=0;i<n/2;i++){
+            s[n-1-i]=s[i];
         }
-
-        if(s.size()%2!=0) mp[s[s.size()/2]]++;
-
-        for(auto it :mp){
-            cout<<it.first<<" : "<<it.second<<endl;
-        }
-        char odd;
-        for(auto it : mp){
-            int val = it.second;
-            for(int i=0;i<val/2;i++) ans += it.first;
-
-            if(val%2!=0){
-                odd = it.first;
-            }
-        }
-        string str = ans;
-
-        reverse(str.begin(),str.end());
-
-        if(s.size()%2!=0){
-             ans += odd;
-        }
-         
-        return ans+str;
-            
-        
-        // return ans+ans2;
+        return s;
     }
 };
