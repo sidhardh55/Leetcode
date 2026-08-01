6ee2b650232e6fa76efdc9d26258e194d0efa782
sidhardh55 <sidhumanchukonda@gmail.com>
2026-08-01 11:10:10 +0530

    Time: 5 ms (78.6%), Space: 25.5 MB (67.82%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,30 +6,33 @@ public:
             hash[word[i]-'a']++;
         }
         int n = word.length();
-        vector<int> sorted;
-        for(int i=0;i<26;i++){
-            if(hash[i]!=0){
-                sorted.push_back(hash[i]);
-            }
-        }
-        sort(sorted.begin(),sorted.end());
-        int mapsize = sorted.size();
-        if(mapsize<=8) return n;
+        // vector<int> sorted;
+        // for(int i=0;i<26;i++){
+        //     if(hash[i]!=0){
+        //         sorted.push_back(hash[i]);
+        //     }
+        // }
+        // sort(sorted.begin(),sorted.end());
+        // int mapsize = sorted.size();
+        sort(hash.begin(),hash.end());
+        // if(mapsize<=8) return n;
         int count = 0;
         int p = 1;
-        for(int i=mapsize-1;i>=0;i--){
+        for(int i=25;i>=0;i--){
 
             if(p<=8){
-                count += sorted[i];
+                count += hash[i];
             }else if(p>8 && p<=16){
-                count += sorted[i]*2;
+                count += hash[i]*2;
             }else if(p>16 && p<=24){
-                count += sorted[i]*3;
+                count += hash[i]*3;
             }else{
-                count += sorted[i]*4;
+                count += hash[i]*4;
             }
-
             p++;
+            if(hash[i]==0){
+                break;
+            }
         }
     
         return count;
