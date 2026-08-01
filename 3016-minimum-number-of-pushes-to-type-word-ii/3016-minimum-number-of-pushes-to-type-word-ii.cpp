e54e52e99704e016504edcb21193b4be114d5642
sidhardh55 <sidhumanchukonda@gmail.com>
2026-08-01 11:29:00 +0530

    Time: 6 ms (74.65%), Space: 25.4 MB (67.82%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,35 +6,13 @@ public:
             hash[word[i]-'a']++;
         }
         int n = word.length();
-        // vector<int> sorted;
-        // for(int i=0;i<26;i++){
-        //     if(hash[i]!=0){
-        //         sorted.push_back(hash[i]);
-        //     }
-        // }
-        // sort(sorted.begin(),sorted.end());
-        // int mapsize = sorted.size();
-        sort(hash.begin(),hash.end());
-        // if(mapsize<=8) return n;
-        int count = 0;
-        int p = 1;
-        for(int i=25;i>=0;i--){
 
-            if(p<=8){
-                count += hash[i];
-            }else if(p>8 && p<=16){
-                count += hash[i]*2;
-            }else if(p>16 && p<=24){
-                count += hash[i]*3;
-            }else{
-                count += hash[i]*4;
-            }
-            p++;
-            if(hash[i]==0){
-                break;
-            }
+        sort(hash.begin(),hash.end(),greater<int>());
+
+        int count = 0;
+        for(int i=0;i<26 && hash[i]>0;i++){
+            count += hash[i]*(i/8 + 1);
         }
-    
         return count;
     }
 };
