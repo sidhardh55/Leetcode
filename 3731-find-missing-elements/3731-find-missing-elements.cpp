e2ed4d8aa83ff1b4e4c4fd92656db7eafa0cdcbe
sidhardh55 <sidhumanchukonda@gmail.com>
2026-08-04 22:54:48 +0530

    Time: 0 ms (100%), Space: 32.1 MB (91.09%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -5,16 +5,12 @@ public:
         sort(nums.begin(),nums.end());
         int j = 0;
         for(int i = nums[0];i<=nums[nums.size()-1];i++){
-            if(j>=nums.size()){
-                vec.push_back(i);
-            }
-            if(i !=nums[j] ){
+            if(i!=nums[j] || j>=nums.size()){
                 vec.push_back(i);
             }else{
                 j++;
             }
         }
-
         return vec;
     }
 };
