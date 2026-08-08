92981e89147b641573c4db2515820925eddd11d5
sidhardh55 <sidhumanchukonda@gmail.com>
2026-08-08 17:38:30 +0530

    Time: 44 ms (41.88%), Space: 26.4 MB (16.45%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         unordered_map<string,int> hm;
-        int j=9;
-        for(int i=0;i<s.length();i++){
-            hm[s.substr(i,10)]++;
-            j++;
-            if(j>=s.length()) break;
-        }
         vector<string> ans;
-        for(auto it : hm){
-            if(it.second>1){
-                ans.push_back(it.first);
+        // int j=9;
+        for(int i=0;i<s.length();i++){
+
+            if(++hm[s.substr(i,10)]==2){
+                ans.push_back(s.substr(i,10));
             }
+            // j++;
+            if(i+9>=s.length()) break;
         }
+       
+
         return ans;
     }
 };
