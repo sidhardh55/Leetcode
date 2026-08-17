cf400264180f920107f847b4287f729cac1a19ef
sidhardh55 <sidhumanchukonda@gmail.com>
2026-08-17 18:51:36 +0530

    Time: 139 ms (53.2%), Space: 158.9 MB (39.65%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int reverse(int num){
-        int res =0;
-        while(num>0){
-            res *= 10;
-            res += num%10;
-            num /=10;
+    int reverseNum(int num){
+        int res = 0;
+        while(num > 0){
+            res = res * 10 + num % 10;
+            num /= 10;
         }
         return res;
     }
+
     int minMirrorPairDistance(vector<int>& nums) {
-        unordered_map<int,int> hm;
+        unordered_map<int,int> hm; 
         int mini = INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            int rev = reverse(nums[i]);
-            if(hm.find(rev)!=hm.end() && nums[i]%10!=0){
-                mini = min(mini,abs(i-hm[rev]));
+        for(int i = 0; i < (int)nums.size(); i++){
+            if(hm.find(nums[i]) != hm.end()){
+                mini = min(mini, i - hm[nums[i]]);
             }
-                int num = nums[i];
-                while(num%10==0){
-                    num /=10;
-                }
-                hm[num] = i;
-            
+            hm[reverseNum(nums[i])] = i;
         }
-        if(mini == INT_MAX) return -1;
-        return mini;
-           
+
+        return mini == INT_MAX ? -1 : mini;
     }
 };
