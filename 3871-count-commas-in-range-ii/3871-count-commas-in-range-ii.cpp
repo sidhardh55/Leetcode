67640a6d3138a1143ac851242a5a0b2fd3dafcf0
sidhardh55 <sidhumanchukonda@gmail.com>
2026-09-21 20:38:29 +0530

    Time: 0 ms (100%), Space: 9 MB (83.65%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    long long countCommas(long long n) {
-        long long ans=0;
-        if(n<1000){
-            return 0;
-        }else if( n>=1000 && n<1000000 ){
-            ans = n-1000+1;
-        }else if(n>=1e6 && n<1e9){
-            ans = 2*(n-1e6+1)+(1e6-1000);
-        }else if(n>=1e9 && n<1e12){
-            ans =3*(n-1e9+1)+2*(1e9-1e6)+(1e6-1000);
-        }else if(n>=1e12 && n<1e15){
-            ans = 4*(n-1e12+1)+3*(1e12-1e9)+2*(1e9-1e6)+(1e6-1000);
-        }else{
-            ans = 4*(n-1e12)+3*(1e12-1e9)+2*(1e9-1e6)+(1e6-1000)+5;
-        }
-        return ans;
+   long long countCommas(long long n) {
+    long long ans = 0;
+    long long power = 1000;
+
+    while (power <= n) {
+        ans += n - power + 1;
+        power *= 1000;
     }
-};
+
+    return ans;
+}};
